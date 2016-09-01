ce2f37384412999b6bac30ee2ac635706fad2a52
cassac <hicassac@gmail.com>
2016-09-01 15:55:32 -0700

    extends layout with navbar and grid system

--- client/app/index.jsx
+++ client/app/index.jsx
@@ -10,7 +10,31 @@ class App extends Component {
   }
   render() {
     return (
-      <div><p>Hello world!!!</p></div>
+      <div>
+        <nav>
+          <div className="nav-wrapper">
+            <a href="#" className="brand-logo">Bru</a>
+            <ul id="nav-mobile" className="right hide-on-med-and-down">
+              <li><a href="#">One</a></li>
+              <li><a href="#">Two</a></li>
+              <li><a href="#">Three</a></li>
+            </ul>
+          </div>
+        </nav>
+        <div className='container'>
+          <div className='row'>
+            <div className="col s4"><img src="http://placehold.it/150x150" /></div>
+            <div className="col s4"><img src="http://placehold.it/150x150" /></div>
+            <div className="col s4"><img src="http://placehold.it/150x150" /></div>
+            <div className="col s4"><img src="http://placehold.it/150x150" /></div>
+            <div className="col s4"><img src="http://placehold.it/150x150" /></div>
+            <div className="col s4"><img src="http://placehold.it/150x150" /></div>
+            <div className="col s4"><img src="http://placehold.it/150x150" /></div>
+            <div className="col s4"><img src="http://placehold.it/150x150" /></div>
+            <div className="col s4"><img src="http://placehold.it/150x150" /></div>
+          </div>
+        </div>
+      </div>
     );
   }
 }
