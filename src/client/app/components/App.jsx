aa9e6027e92d7b706f87a9b380e8de335ba7e58a
cassac <hicassac@gmail.com>
2016-09-06 15:27:16 -0700

    adds navbar

--- src/client/app/components/App.jsx
+++ src/client/app/components/App.jsx
@@ -1,11 +1,35 @@
 import React, {Component} from 'react';
+import { Link } from 'react-router';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>Bru Me</h1>
-        {this.props.children}
+        <nav>
+           <div className="nav-wrapper">
+             <a href="#" className="brand-logo center">Bru</a>
+             <ul id="nav-mobile" className="left hide-on-med-and-down">
+              <li>
+                <Link to='/'>
+                  One
+                </Link>
+              </li>
+              <li>
+                <Link to='/'>
+                  Two
+                </Link>
+              </li>
+              <li>
+                <Link to='/'>
+                  Three
+                </Link>
+              </li>
+             </ul>
+           </div>
+         </nav>
+         <div className='container'>
+          {this.props.children}
+        </div>
       </div>
     )
   }
