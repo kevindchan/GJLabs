b51b427a9b9d9b0db83631c15cef6e8d3de61d80
cassac <hicassac@gmail.com>
2016-09-06 15:28:50 -0700

    adds constructor, state and mapping function for beer selections

--- src/client/app/components/Home.jsx
+++ src/client/app/components/Home.jsx
@@ -1,9 +1,20 @@
 import React, {Component} from 'react';
+import BeerEntry from './BeerEntry.jsx';
 
 export default class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      beers: [1,2,3,4,5,6,7,8,9]
+    }
+  }
   render() {
     return (
-      <div>Hello World</div>
+      <div className='row'>
+        {this.state.beers.map((beer) => {
+          return <BeerEntry key={beer} beer={beer} />;
+        })}
+      </div>
     )
   }
 }
