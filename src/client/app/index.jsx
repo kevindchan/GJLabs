15a6e43d759375c7bdf75c9cbe3215187454c099
cassac <hicassac@gmail.com>
2016-09-06 20:38:10 -0700

    adds catch all route with page not found component and navbar active page attributes

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -4,12 +4,14 @@ import { Router, IndexRoute, Route, browserHistory } from 'react-router';
 import App from './components/App.jsx';
 import Home from './components/Home.jsx';
 import Results from './components/Results.jsx';
+import NotFound from './components/NotFound.jsx';
 
 render((
   <Router history={browserHistory}>
     <Route path="/" component={App}>
       <IndexRoute component={Home}/>
       <Route path='/results' component={Results}/>
+      <Route path="*" component={NotFound}/>
     </Route>
   </Router>
 ), document.getElementById('app'))
