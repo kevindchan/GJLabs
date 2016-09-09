f7bdf54ceab4cebd520972babf1d1acbb09d3491
Kevin Chan <kevindchan@gmail.com>
2016-09-09 14:51:14 -0700

    Fixes navbar button's dissappearing when window size shrinks

--- src/client/app/components/NavBar.jsx
+++ src/client/app/components/NavBar.jsx
@@ -7,7 +7,7 @@ export default ({logoutHandler}) => {
       <nav>
        <div className="nav-wrapper" style={{backgroundColor: '#1d5f8b'}}>
          <a href="#" className="brand-logo center">Bru</a>
-         <ul id="nav-mobile" className="left hide-on-med-and-down">
+         <ul id="nav-mobile" className="left">
           <li>
             <Link activeClassName='active' to='/'>
               Dashboard
@@ -19,7 +19,7 @@ export default ({logoutHandler}) => {
             </Link>
           </li>
          </ul>
-        <ul id="nav-mobile" className="right hide-on-med-and-down">
+        <ul id="nav-mobile" className="right">
           <li><a onClick={logoutHandler} href="#">Logout</a></li>
         </ul>
        </div>
