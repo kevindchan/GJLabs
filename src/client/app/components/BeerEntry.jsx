5e7dfa3dc51cb6cc28afcfa7cdfd15355df6a229
cassac <hicassac@gmail.com>
2016-09-09 12:18:39 -0700

    adds attributes to beer entry

--- src/client/app/components/BeerEntry.jsx
+++ src/client/app/components/BeerEntry.jsx
@@ -10,7 +10,17 @@ export default ({beer}) => {
         <div className="card-content activator valign-wrapper">
           <span className="card-title activator grey-text text-darken-4" style={{fontSize: '18px', lineHeight: '100%'}}>{beer.style.name}</span><i className="material-icons right">more_vert</i>
           <p className='center-align valign'>
-            <input type="checkbox" className="filled-in right" id={beer.id} value={beer.id} />
+            <input 
+              type="checkbox" 
+              className="filled-in right" 
+              id={beer.id} 
+              value={beer.id}
+              data-icon-url={beer.labels.icon} 
+              data-brewery-name={beer}
+              data-beer-name={beer.name}
+              data-style-family={`style-family`}
+              data-style-id={beer.styleId}
+            />
             <label htmlFor={beer.id}></label>
           </p>
         </div>
