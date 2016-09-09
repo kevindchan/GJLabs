4e4f7b3dc4b333bd2a875447cbab406cb76e1ab7
cassac <hicassac@gmail.com>
2016-09-09 00:55:42 -0700

    moves user creation to auth controller

--- src/server/controllers/UsersController.js
+++ src/server/controllers/UsersController.js
@@ -11,21 +11,5 @@ module.exports = {
     .catch((err) => {
       res.json({results: err, message: 'Error fetching resources'})
     })
-  },
-  post: (req, res) => {
-    const data = req.body;
-    User.create({
-      username: data.username,
-      firstName: data.firstName,
-      lastName: data.lastName,
-      email: data.email,
-      password: data.password
-    })
-    .then((user) => {
-      res.json({results: user, message: 'Resource created'});
-    })
-    .catch((err) => {
-      res.json({results: err, message: 'Resource not created'});
-    })
   }
 }
