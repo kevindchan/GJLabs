ca6e2e3808c8b9e6834b539b9a52d5a1ddfba723
Kevin Chan <kevindchan@gmail.com>
2016-09-09 16:41:46 -0700

    Refactors to include likeHandler

--- src/client/app/components/Beerlog.jsx
+++ src/client/app/components/Beerlog.jsx
@@ -1,12 +1,12 @@
 import React, {Component} from 'react';
 import BeerlogEntry from './BeerlogEntry.jsx';
 
-export default ({beers}) => {
+export default ({likeHandler, beers}) => {
   return (
     <ul className="collection with-header">
       <li className="collection-header" style={{backgroundColor: "#578fb4", color: "white"}}><h5 className="center">Beerlog</h5></li>
       {beers.map((beer) => {
-        return <BeerlogEntry key={beer.id} beer={beer} />
+        return <BeerlogEntry key={beer.id} beer={beer} likeHandler={likeHandler} />
       })}
     </ul>    
   )
