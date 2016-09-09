d982a0ccf2545ad12eeebf14dab1b15fb8b6cacb
Kevin Chan <kevindchan@gmail.com>
2016-09-09 13:50:45 -0700

    Changes navbar to fixed, color scheme to blue, and truncates beerlog beer description

--- src/client/app/components/Beerlog.jsx
+++ src/client/app/components/Beerlog.jsx
@@ -3,7 +3,8 @@ import BeerlogEntry from './BeerlogEntry.jsx';
 
 export default ({beers}) => {
   return (
-    <ul className="collection">
+    <ul className="collection with-header">
+      <li className="collection-header" style={{backgroundColor: "#578fb4", color: "white"}}><h5 className="center">Beerlog</h5></li>
       {beers.map((beer) => {
         return <BeerlogEntry key={beer.id} beer={beer} />
       })}
--- src/client/app/components/BeerlogEntry.jsx
+++ src/client/app/components/BeerlogEntry.jsx
@@ -5,9 +5,8 @@ export default ({beer}) => {
     <li className="collection-item avatar">
       <img src={beer.labels.medium} alt="" className="circle" />
       <span className="title">{beer.style.name}</span>
-      <p>Brewery: {beer.style.name} <br />
-         {beer.description}
-      </p>
+      <p style={{color: 'blue'}}>Brewery: {beer.style.name}</p>
+      <p className="truncate">{beer.description}</p>
       <a href="#!" className="secondary-content"><i className="material-icons">grade</i></a>
     </li>
   )
