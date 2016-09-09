9360dd17049165a8c1eeda78b73329214dd13da0
cassac <hicassac@gmail.com>
2016-09-08 23:55:23 -0700

    completes PUT method for user beer log api endpoint

--- src/server/controllers/UserBeerLogController.js
+++ src/server/controllers/UserBeerLogController.js
@@ -67,7 +67,38 @@ module.exports = {
     })
   },
   put: (req, res) => {
-    res.json('beerlog controller put request')
+    const data = req.body;
+    const userId = req.params.userId;
+    User.findById(userId)
+    .then((user) => {
+      return user.getBeers({
+        where: {
+          beerId: data.beerId
+        }
+      })
+      .then((beer) => {
+        if (!beer.length) {
+          return res.json({results: null, message: 'Beer not found in beer log.'})
+        } else {
+          const beerLogId = beer[0].dataValues.beerlog.id;
+          BeerLog.update({
+            liked: data.liked,
+            rating: data.rating
+          }, {
+            where: {
+              id: beerLogId
+            }
+          })
+          .then((beer) => {
+            return beer;
+          })
+          return res.json({results: beer, message: 'Beer updated in beer log.'})
+        }
+      })
+    })
+    .catch((err) => {
+      res.json({results: err, message: 'Error retrieving resource.'})
+    })
   },
   delete: (req, res) => {
   	const beerId = req.params.beerId;
