b9ee62b3e0acd244c40c661bf6f8c6f406655cd8
SeanKinzler <sfk27@case.edu>
2016-09-10 10:24:33 -0700

    added style family test

--- src/Tests/tests.js
+++ src/Tests/tests.js
@@ -1,6 +1,6 @@
-var expect = require('chai').expect
-var beerList = require('/../../beerdata/multipleExample.js')
-var aleGraph = require('/../server/graph/node.js').aleGraph
+var expect = require('chai').expect;
+var beerList = require('../../beerdata/multipleExample.js');
+var aleGraph = require('../server/graph/node.js').aleGraph;
 describe('Graph Tests', function() {
   it('Should initialize the correct Graph', function() {
     expect(aleGraph.nodes.length).to.equal(9)
@@ -22,3 +22,10 @@ describe('Node Tests', function() {
   })
 
 });
+
+describe('Style Families', function() {
+  it('Should initialize styleFamilies object', function() {
+    var styleFamilies = require('../../beerdata/styleFamilies.js')
+    expect(Object.keys(styleFamilies).length).to.equal(14);
+  })
+})
