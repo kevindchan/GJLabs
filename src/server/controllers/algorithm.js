5aed8a429559672b1a2b385bd0cd5676d80c0ac4
austinmr <austin.riedel@gmail.com>
2016-09-09 19:27:41 -0700

    Implement the merge functionality to combine the result of the graph map and characteristic search functionalities within the algorithm.

--- src/server/controllers/algorithm.js
+++ src/server/controllers/algorithm.js
@@ -2,7 +2,8 @@ var graphs = require('../graph/node.js');
 var aleGraph = graphs.aleGraph; 
 var lagerGraph = graphs.lagerGraph; 
 var styleFamilies = require('../../../beerdata/styleFamilies.js'); 
-// var stylesData = require('../../../beerdata/styles.json'); 
+var stylesData = require('../../../beerdata/styles.js'); 
+var _ = require('underscore'); 
 
 var beerList = require('../../../beerdata/multipleExample.js'); 
 
@@ -10,7 +11,7 @@ var beerList = require('../../../beerdata/multipleExample.js');
 var algorithm = function(beerList) {
 	var beerListStylesId = beerList.map((beer) => beer.styleId); 
 	var beerListStyleFamilyIds = beerList.map((beer) => beer.styleFamilyId);
-	console.log('STYLE FAMILY IDS: ', beerListStyleFamilyIds); 
+	// console.log('STYLE FAMILY IDS: ', beerListStyleFamilyIds); 
 
 	// Step 1. Determine the Specific Case: 
 
@@ -35,6 +36,7 @@ var algorithm = function(beerList) {
 	var unselectedRatio = .20;
 	var currentStyleId = getCurrentNode(beerList[0]).styleFamilyId; 
 	var currentStyleFamily = getCurrentNode(beerList[0]).styleFamily; 
+	// console.log(currentStyleFamily); 
 
 	var total = beerListStylesId.length; 
 	var unselected = 0; 
@@ -51,6 +53,8 @@ var algorithm = function(beerList) {
 			unselected++; 
 		}; 
 	}); 
+
+	// console.log('SELECTIONS PER STYLE: ', selectionsPerStyle); 
 
 	var styleKeys = Object.keys(selectionsPerStyle); 
 	styleKeys.forEach((key) => {
@@ -91,10 +95,9 @@ var algorithm = function(beerList) {
 	/////// CASE 2: NOT ALL THE SAME CATEGORY ///////////////// 
 
 	var overLapScoresObject = getBeerOverlapScores(beerList); 
-	console.log(overLapScoresObject); 
 
 	//////// COMPARE TO CURRENT NODES ////////
-	// console.log(algorithmResult); 
+	// console.log('ALGORITHM RESULT: \n', algorithmResult); 
 
 	var primaryCategory = calculatePrimaryCategory(beerListStyleFamilyIds); 
 	primaryCategoryData = stylesData[primaryCategory]; 
@@ -103,14 +106,24 @@ var algorithm = function(beerList) {
 
 	var comparisonData = calculateComparison(algorithmResult, primaryCategoryCharacteristics); 
 	var currentNode = aleGraph.storage[primaryCategory]; 
-	var adjacentNodes = calculateComparableNodes(comparisonData, currentNode); 
+	var similarNodes = calculateComparableNodes(comparisonData, currentNode); 
 
 	// console.log(comparisonData); 
 	// console.log(currentNode);
 	// console.log(adjacentNodes); 
 
-	return algorithmResult; 
-	
+	var searchCategoryScores = calculateSearchCategoryScores(overLapScoresObject, similarNodes); 
+	console.log('SEARCH CATEGORY SCORES', searchCategoryScores); 
+
+	searchCategoryScores = convertToCount(searchCategoryScores, 50); 
+	console.log('SEARCH CATEGORY COUNT', searchCategoryScores); 
+
+	selectionsPerStyleId = algorithmSelectionsPerStyle(searchCategoryScores, beerListStylesId); 
+	console.log(selectionsPerStyleId); 
+
+	// var singleSelection = singleStyleCalculator(19, 25, beerListStylesId); 
+
+	// return algorithmResult; 
 }; 
 
 var getBeerOverlapScores = function(beerList) {
@@ -118,7 +131,7 @@ var getBeerOverlapScores = function(beerList) {
 	for (var i = 0; i < beerList.length; i++) {
 		var currentNode = getCurrentNode(beerList[i]);
 		var adjFams = currentNode.allAdjacent();
-		console.log('adjFams: ', adjFams);
+		// console.log('adjFams: ', adjFams);
 		for (var j = 0; j < adjFams.length; j++) {
 			if (adjFams[j] !== undefined && stylesScores[adjFams[j]] === undefined) {
 				stylesScores[adjFams[j]] = 1;
@@ -127,7 +140,7 @@ var getBeerOverlapScores = function(beerList) {
 			}
 		}
 	}
-	console.log('Scores: ', stylesScores);
+	// console.log('Scores: ', stylesScores);
 	return stylesScores;
 }
 
@@ -157,6 +170,8 @@ var categoryConfirm = function(styleFamily, styleIdArray) {
 
 var selectionPerStyleCalculator = function(stylePercent, listLength, ratio, unselectedCount) {
 	if (stylePercent > 0) {
+		console.log('percent: ', stylePercent); 
+		console.log('listlength: ', listLength); 
 		return Math.floor(ratio * listLength * stylePercent); 
 	} else {
 		return Math.floor((1-ratio) * listLength / unselectedCount); 
@@ -252,6 +267,114 @@ var calculatePrimaryCategory = function(beerListStylesId) {
 		}; 
 	}); 
 	return categoryCount['maxStyle'] || beerListStylesId[0]; 
+}
+
+var calculateSearchCategoryScores = function (overLapScoresObject, similarNodes) {
+	if (!similarNodes) {
+		return calculateTopValues(overLapScoresObject); 
+	} else {
+		similarNodes.forEach((node) => {
+			overLapScoresObject[node] = overLapScoresObject[primaryCategory]; 
+			// beerListStylesId.push(node); 
+		}); 
+		return calculateTopThree(overLapScoresObject); 
+	}
+}; 
+
+var calculateTopThree = function (object) {
+	var resultObject = {}; 
+	var sortedArray = []; 
+	var keys = Object.keys(object); 
+	keys.forEach((key) => {
+		sortedArray.push([key, object[key]]); 
+	}); 
+	sortedArray.sort(function(a,b) {
+		return a[1] - b[1]; 
+	}); 
+	resultObject[1] = sortedArray[sortedArray.length - 1]; 
+	resultObject[2] = sortedArray[sortedArray.length - 2]; 
+	resultObject[3] = sortedArray[sortedArray.length - 3]; 
+	resultObject['total'] = resultObject[1][1] + resultObject[2][1] + resultObject[3][1]; 
+	return resultObject; 
+}
+
+var convertToCount = function (categoryObject, totalCountList) {
+	var total = categoryObject.total; 
+	delete categoryObject.total; 
+	var keys = Object.keys(categoryObject); 
+	var resultObject = {}
+	keys.forEach((key) => {
+		resultObject[categoryObject[key][0]] = Math.floor( (categoryObject[key][1] / total) * totalCountList);  
+	}); 
+	return resultObject; 
+}
+
+var algorithmSelectionsPerStyle = function (categoryCountObject, beerListStylesId) {
+	var selectionsPerStyleId = {}; 
+	var allStyleFamilies = []; 
+	var unselected = 0; 
+	var total = 0; 
+	var preferredStyleIds = beerListStylesId.slice(); 
+	var result = {}; 
+
+	// This is to add the values that we are looking for to the preferredStyles so they will receive more distribution. 
+	var keys = Object.keys(categoryCountObject); 
+
+	keys.forEach((key) => {
+		if (preferredStyleIds.indexOf(key) === -1) {
+			preferredStyleIds.push(parseInt(key)); 
+		} 
+	}); 
+
+	keys.forEach((key) => {
+		selectionsPerStyleId[key] = singleStyleCalculator(key, categoryCountObject[key], preferredStyleIds); 
+	}); 
+
+	keys.forEach((key) => {
+		_.extend(result, selectionsPerStyleId[key]);
+	}); 
+
+	return result; 
+}
+
+var singleStyleCalculator = function (styleId, totalCountForList, preferredStyleIds) {
+	var selectedRatio = .75; 
+	var unselectedRatio = .25; 
+	var selectionsPerStyleId = {}; 
+	var unselected = 0; 
+	var total = 0; 
+	var styleFamily = styleFamilies[styleId]; 
+
+	var preferredInCategory = []; 
+
+	preferredStyleIds.forEach(function(styleId) {
+		if (styleFamily.indexOf(styleId) !== -1) {
+			preferredInCategory.push(styleId); 
+		}
+	}); 
+
+	preferredInCategory.forEach((style) => {
+		selectionsPerStyleId[style] = selectionsPerStyleId[style] + 1 || 1;
+		total++;  
+	}); 
+
+	styleFamily.forEach((style) => {
+		if (selectionsPerStyleId[style] === undefined) {
+			selectionsPerStyleId[style] = 0; 
+			unselected++; 
+		}; 
+	}); 
+
+	var styleKeys = Object.keys(selectionsPerStyleId); 
+	styleKeys.forEach((key) => {
+		var styleCount = selectionsPerStyleId[key]; 
+		var stylePercent = styleCount / total; 
+		var test = selectionPerStyleCalculator(stylePercent, totalCountForList, selectedRatio, unselected);
+		selectionsPerStyleId[key] = test; 
+	}); 
+
+	return selectionsPerStyleId; 
+
 }
 
 module.exports = algorithm; 
