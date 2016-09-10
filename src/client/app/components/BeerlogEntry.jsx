cb47b381217f91c456d571c032280eeb6a9be23e
cassac <hicassac@gmail.com>
2016-09-09 18:30:20 -0700

    adds description to beer display

--- src/client/app/components/BeerlogEntry.jsx
+++ src/client/app/components/BeerlogEntry.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 
-export default ({likeHandler, beer}) => {
+export default ({beer}) => {
   return (
     <li className="collection-item avatar">
       <img src={beer.iconUrl} alt="" className="circle" />
@@ -11,8 +11,7 @@ export default ({likeHandler, beer}) => {
           <label htmlFor={beer.beerId}>Like</label>
         </p>
       </form>
-      <p><span className="blue-text">Brewery:</span> {beer.breweryName}</p>
-      <p><span className="blue-text">Description: </span>Beer description...</p>
+      <p><span className="blue-text">Description: </span>{beer.description}</p>
     </li>
   )
 }
