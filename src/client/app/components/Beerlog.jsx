988961fa7fffae200f46f7d29711f83649851d88
cassac <hicassac@gmail.com>
2016-09-09 18:48:32 -0700

    adjusts styling for beerlog

--- src/client/app/components/Beerlog.jsx
+++ src/client/app/components/Beerlog.jsx
@@ -3,11 +3,17 @@ import BeerlogEntry from './BeerlogEntry.jsx';
 
 export default ({beerlog}) => {
   return (
-    <ul className="collection with-header">
-      <li className="collection-header" style={{backgroundColor: "#578fb4", color: "white"}}><h5 className="center">Beerlog</h5></li>
-      {beerlog.map((beer) => {
-        return <BeerlogEntry key={beer.beerId} beer={beer} />
-      })}
-    </ul>    
+		<div>
+	    <ul className="collection with-header">
+	      <li className="collection-header" style={{backgroundColor: "#578fb4", color: "white"}}>
+	      	<h5 className="center">Beerlog</h5>
+	      </li>
+	    </ul>
+		  <ul className="collapsible popout" data-collapsible="accordion">
+		  {beerlog.map((beer) => {
+		    return <BeerlogEntry key={beer.beerId} beer={beer} />
+		  })}
+	    </ul>
+		</div>
   )
 }
--- src/client/app/components/BeerlogEntry.jsx
+++ src/client/app/components/BeerlogEntry.jsx
@@ -2,16 +2,20 @@ import React, {Component} from 'react';
 
 export default ({beer}) => {
   return (
-    <li className="collection-item avatar">
-      <img src={beer.iconUrl} alt="" className="circle" />
-      <span className="title">{beer.beerName}</span>
-      <form className="right" action="#">
-        <p>
-          <input type="checkbox" id={beer.beerId} />
-          <label htmlFor={beer.beerId}>Like</label>
-        </p>
-      </form>
-      <p><span className="blue-text">Description: </span>{beer.description}</p>
+    <li>
+      <div className="collapsible-header">
+        <img src={beer.iconUrl} alt="" className="circle" />
+        <span className="title">{beer.beerName}</span>
+        <form className="right" action="#">
+          <p>
+            <input type="checkbox" id={beer.beerId} />
+            <label htmlFor={beer.beerId}>Like</label>
+          </p>
+        </form>
+      </div>
+      <div className="collapsible-body">
+        <p><span className="blue-text">Description: </span>{beer.description}</p>
+      </div>
     </li>
   )
 }
