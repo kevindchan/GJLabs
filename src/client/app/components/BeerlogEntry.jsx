eea50c18732b6a94ce54a51b86239446b9d2f50d
cassac <hicassac@gmail.com>
2016-09-09 17:21:26 -0700

    resolves conflict

--- src/client/app/components/BeerlogEntry.jsx
+++ src/client/app/components/BeerlogEntry.jsx
@@ -3,16 +3,16 @@ import React, {Component} from 'react';
 export default ({likeHandler, beer}) => {
   return (
     <li className="collection-item avatar">
-      <img src={beer.labels.medium} alt="" className="circle" />
-      <span className="title">{beer.style.name}</span>
+      <img src={beer.iconUrl} alt="" className="circle" />
+      <span className="title">{beer.beerName}</span>
       <form className="right" action="#">
         <p>
-          <input type="checkbox" id={beer.id} onChange={likeHandler}/>
-          <label htmlFor={beer.id}>Like</label>
+          <input type="checkbox" id={beer.beerId} />
+          <label htmlFor={beer.beerId}>Like</label>
         </p>
       </form>
-      <p><span className="blue-text">Brewery:</span> {beer.style.name}</p>
-      <p><span className="blue-text">Description: </span>{beer.description}</p>
+      <p><span className="blue-text">Brewery:</span> {beer.breweryName}</p>
+      <p><span className="blue-text">Description: </span>Beer description...</p>
     </li>
   )
 }
