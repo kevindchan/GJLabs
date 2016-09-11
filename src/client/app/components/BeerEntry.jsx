8d63cf2f3e9a1783d5f964c59ecaec84fe813d74
cassac <hicassac@gmail.com>
2016-09-11 12:26:14 -0700

    assigns props to state

--- src/client/app/components/BeerEntry.jsx
+++ src/client/app/components/BeerEntry.jsx
@@ -4,17 +4,10 @@ import React, {Component} from 'react';
 export default class BeerEntry extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      beer: {}
-    }
+    this.state = {}
   }
-  componentWillMount() {
-    this.setState({
-      beer: this.props.beer
-    })
-  }
-  imageSelector(size) {
-    const src = this.state.beer.labels !== undefined ? this.state.beer.labels[size] : 
+  imageSelector(beer, size) {
+    const src = beer.labels !== undefined ? beer.labels[size] : 
       'http://images.huffingtonpost.com/2016-01-26-1453821995-8643361-beermain.jpg';
     return src;
   }
@@ -22,22 +15,39 @@ export default class BeerEntry extends Component {
     const name = beer.breweries !== undefined ? beer.breweries[0].name : '';
     return name;
   }
+  componentWillMount() {
+    this.setState({
+      beerId: this.props.beer.id,
+      description: this.props.beer.description,
+      iconUrl: this.imageSelector(this.props.beer, 'icon'),
+      imageUrl: this.imageSelector(this.props.beer, 'medium'),
+      breweryName: this.breweryName(this.props.beer),
+      beerName: this.props.beer.name,
+      styleFamily: this.props.beer.styleFamily,
+      styleFamilyId: this.props.beer.styleFamilyId,
+      styleId: this.props.beer.styleId,
+      abv: this.props.beer.abv,
+      ibu: this.props.beer.ibu,
+      srm: this.props.beer.srmId,
+      styleName: this.props.beer.style.name
+    })
+  }
   likeHandler(e) {
     const userId = localStorage.userId;
     const liked = e.target;
     const styleFamily = $(liked).data('style-family');
     const beer = {
-      beerId: $(liked).val(),
-      description: $(liked).data('description'),
-      iconUrl: $(liked).data('icon-url'),
-      breweryName: $(liked).data('brewery-name'),
-      beerName: $(liked).data('beer-name'),
-      styleFamily: styleFamily,
-      styleFamilyId: $(liked).data('style-family-id'),
-      styleId: $(liked).data('style-id'),
-      abv: $(liked).data('abv'),
-      ibu: $(liked).data('ibu'),
-      srm: $(liked).data('srm')
+      beerId: this.state.beerId,
+      description: this.state.description,
+      iconUrl: this.state.iconUrl,
+      breweryName: this.state.breweryName,
+      beerName: this.state.beerName,
+      styleFamily: JSON.stringify(this.state.styleFamily),
+      styleFamilyId: this.state.styleFamilyId,
+      styleId: this.state.styleId,
+      abv: this.state.abv,
+      ibu: this.state.ibu,
+      srm: this.state.srm
     }
     axios.post('/api/users/' + userId + '/beers', beer)
     .then((response) => {
@@ -52,40 +62,30 @@ export default class BeerEntry extends Component {
       <div className="col s4">
         <div className="card small">
           <div className="card-image">
-            <img className='responsive-img activator' src={this.imageSelector('medium')} />
+            <img className='responsive-img activator' src={this.state.imageUrl} />
           </div>
           <div className="card-content activator valign-wrapper">
             <span className="card-title activator grey-text text-
             darken-4" 
-              style={{fontSize: '18px', lineHeight: '100%'}}>{this.state.beer.name}
+              style={{fontSize: '18px', lineHeight: '100%'}}>{this.state.beerName}
             </span><i className="material-icons right">more_vert</i>
             <p className='center-align valign'>
               <input 
                 type="checkbox" 
                 className="filled-in right" 
-                id={this.state.beer.id} 
-                value={this.state.beer.id}
-                data-description={this.state.beer.description}
-                data-icon-url={this.imageSelector('icon')} 
-                data-brewery-name={this.breweryName(this.state.beer)}
-                data-beer-name={this.state.beer.name}
-                data-style-family={this.state.beer.styleFamily}
-                data-style-family-id={this.state.beer.styleFamilyId}
-                data-style-id={this.state.beer.styleId}
-                data-abv={this.state.beer.abv}
-                data-ibu={this.state.beer.ibu}
-                data-srm={this.state.beer.srmId}
+                id={this.state.beerId} 
+                value={this.state.beerId}
                 onChange={this.likeHandler.bind(this)}
               />
-              <label htmlFor={this.state.beer.id}></label>
+              <label htmlFor={this.state.beerId}></label>
             </p>
           </div>
           <div className="card-reveal">
             <span className="card-title grey-text text-darken-4">
-              {this.state.beer.style.name}
+              {this.state.styleName}
               <i className="material-icons right">close</i>
             </span>
-            <p>{this.state.beer.description}</p>
+            <p>{this.state.description}</p>
           </div>
         </div>
       </div>
