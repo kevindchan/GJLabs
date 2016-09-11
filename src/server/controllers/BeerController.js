243f92ea9838164793e3e8376bb30c8d7c926e13
cassac <hicassac@gmail.com>
2016-09-10 21:30:03 -0700

    updates beer controller to match beer model

--- src/server/controllers/BeerController.js
+++ src/server/controllers/BeerController.js
@@ -16,7 +16,14 @@ module.exports = {
 			beerId: data.beerId,
 			ibu: data.ibu,
 			srm: data.srm,
-			abv: data.abv
+			abv: data.abv,
+			styleId: data.styleId,
+			styleFamily: data.styleFamily,
+			styleFamilyId: data.styleFamilyId,
+			breweryName: data.breweryName,
+			beerName: data.beerName,
+			iconUrl: data.iconUrl,
+			description: data.description
 		})
 		.then((beer) => {
 			res.json({results: beer, message: 'Resource created.'});
