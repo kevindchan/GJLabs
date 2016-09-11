77b0ee02910a8e141926e59a463609865eb3b6f9
austinmr <austin.riedel@gmail.com>
2016-09-11 10:32:13 -0700

    Modify beerdata files to work with changes to algorithm
    Modify paleAleSample data to include styleFamilyId property
    Add styleFamilyNames.js file to allow style name lookup within algorithm

---
+++ beerdata/styleFamilyNames.js
@@ -0,0 +1,18 @@
+module.exports = {
+  //lagers
+  94: 'Light Lagers', // Light Lager - #1
+  75:  'Pilsners', // Pilsner - #2
+  80: 'European Amber Ales', // European Amber - #6
+  103: 'Dark Lagers', // Dark Lager 
+  88: 'Bocks', // Bock 
+  //ales
+  1: 'English Ales', // English Pale
+  22: 'Irish Ales', // Irish Ale 
+  25: 'American Ales', // American Ale - #4 
+  11: 'English Brown Ales', // English Brown - #7 
+  19: 'Porters', // Porter - #8 
+  23: 'Stouts', // Stout - #9
+  30: 'IPA', // IPA - #5 
+  49: 'German Wheat Beers', // German Wheat 
+  65: 'Belgian-Style Ales' // Belgian - #3
+}
