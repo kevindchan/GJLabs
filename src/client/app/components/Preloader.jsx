89c168acb09ccc69fd9aacb17f6a8d54c046e704
cassac <hicassac@gmail.com>
2016-09-12 14:58:31 -0700

    updates preloader to contain modal

--- src/client/app/components/Preloader.jsx
+++ src/client/app/components/Preloader.jsx
@@ -4,11 +4,23 @@ export default () => {
   return (
     <div id="modal1" className="modal modal-fixed-footer">
       <div className="modal-content">
-        <h4>Modal Header</h4>
-        <p>A bunch of text</p>
-      </div>
-      <div className="modal-footer">
-        <a href="#!" className="modal-action modal-close waves-effect waves-green btn-flat ">Agree</a>
+        <h1 className='center-align'>Executing Algorithm</h1>
+          <div className='center-align'>
+            <div id="preloader" className="preloader-wrapper big active">
+              <div className="spinner-layer spinner-green-only">
+               <div className="circle-clipper left">
+                 <div className="circle"></div>
+               </div>
+               <div className="gap-patch">
+                 <div className="circle"></div>
+               </div>
+               <div className="circle-clipper right">
+                 <div className="circle"></div>
+               </div>
+              </div>
+            </div>   
+          </div>  
+        <h4 className='center-align'>Finding a beer based on your taste profile.</h4>
       </div>
     </div>
   );
